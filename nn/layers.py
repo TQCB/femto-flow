d936af7f9b5b506a7cd553a9eef3056e4a732fd6
Raphaël Rialland <raphael.pb.rialland@gmail.com>
2024-12-07 19:56:41 +0100

    updated indent size to 2

--- nn/layers.py
+++ nn/layers.py
@@ -18,60 +18,68 @@ import numpy as np
 ################################################################################
 
 class Layer:
-    def __init__(self, input_dim, output_dim):
-        self.weights = np.random.uniform(-1, 1, size=(input_dim, output_dim))
-        self.bias = np.random.uniform(-1, 1,size=output_dim) - 0.5
+  def __init__(self, input_dim, output_dim):
+    self.weights = np.random.uniform(-1, 1, size=(input_dim, output_dim))
+    self.bias = np.random.uniform(-1, 1,size=output_dim) - 0.5
     
-    # compute h(X) = Y
-    def forward_propagation(self, input):
-        self.input = input
-        self.output = np.dot(self.input, self.weights) + self.bias
-        return self.output
+  # compute h(X) = Y
+  def forward_propagation(self, input):
+    self.input = input
+    self.output = np.dot(self.input, self.weights) + self.bias
+    return self.output
 
-    # compute dE/dX for dE/dY and update dX
-    def backward_propagation(self, output_error, learning_rate):
-        input_error = np.dot(output_error, self.weights.T)
-        weights_error = np.dot(self.input.T, output_error)
+  # compute dE/dX for dE/dY and update dX
+  def backward_propagation(self, output_error, learning_rate):
+    input_error = np.dot(output_error, self.weights.T)
+    weights_error = np.dot(self.input.T, output_error)
         
-        self.weights -= learning_rate * weights_error
-        self.bias -= learning_rate * output_error
+    self.weights -= learning_rate * weights_error
+    self.bias -= learning_rate * output_error
         
-        return input_error
+    return input_error
     
 class ActivationLayer(Layer):
-    def __init__(self, activation):
-        self.activation = activation()
+  def __init__(self, activation):
+    self.activation = activation()
 
-    # compute h(X) = Y
-    def forward_propagation(self, input):
-        self.input = input
-        self.output = self.activation.forward(self.input)
-        return self.output
+  # compute h(X) = Y
+  def forward_propagation(self, input):
+    self.input = input
+    self.output = self.activation.forward(self.input)
+    return self.output
     
     # compute dE/dX for dE/dY 
-    def backward_propagation(self, output_error, learning_rate):
-        return self.activation.backward(self.input) * output_error
+  def backward_propagation(self, output_error, learning_rate):
+    return self.activation.backward(self.input) * output_error
     
 class FCLayer(Layer):
-    def __init__(self, input_dim, output_dim):
-        self.weights = np.random.rand(input_dim, output_dim) - 0.5
-        self.bias = np.random.rand(1, output_dim) - 0.5
+  def __init__(self, input_dim, output_dim):
+    self.weights = np.random.rand(input_dim, output_dim) - 0.5
+    self.bias = np.random.rand(1, output_dim) - 0.5
         
-    def forward_propagation(self, input):
-        self.input = input
-        self.output = np.dot(self.input, self.weights) + self.bias
-        return self.output
+  def forward_propagation(self, input):
+    self.input = input
+    self.output = np.dot(self.input, self.weights) + self.bias
+    return self.output
     
-    def backward_propagation(self, output_error, learning_rate):
-        input_error = np.dot(output_error, self.weights.T)
-        weights_error = np.dot(self.input.T, output_error)
+  def backward_propagation(self, output_error, learning_rate):
+    input_error = np.dot(output_error, self.weights.T)
+    weights_error = np.dot(self.input.T, output_error)
         
-        self.weights -= learning_rate * weights_error
-        self.bias -= learning_rate * output_error
-        return input_error
+    self.weights -= learning_rate * weights_error
+    self.bias -= learning_rate * output_error
+    return input_error
     
 class MultiHeadAttention(Layer):
-    def __init__(self, n_heads, n_dim):
-        self.q_weights =
-        self.k_weights
-        self.v_weights
+  """
+  Multi Head Self Attention for Dense networks.
+    
+  Args:
+        
+  """
+    
+  def __init__(self, input_dim, n_dim, n_heads):
+        
+    self.n_heads = n_heads
+    self.n_dim = n_dim
+    self.head_dim = n_dim // n_heads
