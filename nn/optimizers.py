960b60cc1cda5f720d54660db9abc50ab6cf0f5b
Raphaël Rialland <raphael.pb.rialland@gmail.com>
2024-12-15 12:40:36 +0100

    added linear cycling learning rate schedule

--- nn/optimizers.py
+++ nn/optimizers.py
@@ -29,3 +29,35 @@ class ExponentialDecaySchedule(LearningRateSchedule):
     
     def increment(self):
         self.lr *= self.decay_rate
+        
+class LinearCycleSchedule(LearningRateSchedule):
+    def __init__(self, min_lr, max_lr, cycle_rate):
+        # Set initial lr to minimum
+        self.lr = min_lr
+        
+        # Set bounds
+        self.min = min_lr
+        self.max = max_lr
+        
+        # Set linear increase/decrease rate based on phase
+        self.rate = cycle_rate
+        self.phase = 1
+        
+    def __call__(self):
+        self.increment()
+        
+        # If learning rate hits max, inverse phase and return max
+        if self.lr >= self.max:
+            self.phase = -1
+            return self.max
+        
+        # If learning rate hits min, inverse phase and return min
+        if self.lr <= self.min:
+            self.phase = 1
+            return self.min
+        
+        return self.lr
+    
+    def increment(self):
+        # Update lr positively or negatively by rate based on phase
+        self.lr *= 1 + (self.phase*self.rate)
