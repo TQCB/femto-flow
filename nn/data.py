7c09b2f0857a90361b20b1d5ffab001391adcfeb
Raphael <raphael.pb.rialland@gmail.com>
2024-12-16 18:17:47 +0100

    added condition to only remove extra lines if they exist

--- nn/data.py
+++ nn/data.py
@@ -2,10 +2,11 @@ import numpy as np
 
 def batch(data, batch_size):
   '''
-  Transform data from (samples, features) into (batch, samples, features)
+  Transform data from (sample, feature) into (batch, sample, feature)
   '''
   # Find out how many extra samples we have and remove them
   extra = data.shape[0] % batch_size
-  data = data[:-extra,:]
+  if extra != 0:
+    data = data[:-extra,:]
   
   return data.reshape((-1, batch_size, data.shape[1]))
