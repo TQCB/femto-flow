4079f2abe909535658ef2a5afae002cacb0f5271
Raphael <raphael.pb.rialland@gmail.com>
2024-12-18 19:47:18 +0100

    removed unnecessary linebreak

--- nn/losses.py
+++ nn/losses.py
@@ -6,7 +6,6 @@ def mse(y_true, y_pred):
 def d_mse(y_true, y_pred):
     return 2*(y_pred-y_true)/y_true.shape[0]
     
-    
 def bce(y_true, y_pred):
     return -np.mean((y_true * np.log(y_pred)) + ((1-y_true) * np.log((1-y_pred))))
     
