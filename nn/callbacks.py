b475e61f26e32916477f2658bd4d02be90160bf0
Raphael <raphael.pb.rialland@gmail.com>
2024-12-23 17:56:06 +0100

    created callback module with save on progress callback to be used during fit on model

---
+++ nn/callbacks.py
@@ -0,0 +1,17 @@
+import pickle
+
+class SaveOnProgressCallback:
+  def __init__(self, save_path):
+    """Callback that takes a model as an input and saves it if the error has decreased."""
+    self.min_error = 99999
+    self.save_count = 0
+    self.save_path = save_path
+  
+  def __call__(self, model):
+    if model.error < self.min_error:
+      current_save_path = self.save_path + str(self.save_count) + ".pkl"
+      with open(current_save_path, 'wb') as f:
+        pickle.dump(model, f)
+      self.save_count += 1
+    else:
+      pass
