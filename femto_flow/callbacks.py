96b7581535370b1b91f2dc9b19c4feeba40915a5
Raphaël Rialland <raphael.pb.rialland@gmail.com>
2025-01-20 21:44:15 +0100

    updated callback code to use validation error and fixed error where min error would not update

--- femto_flow/callbacks.py
+++ femto_flow/callbacks.py
@@ -3,12 +3,16 @@ import pickle
 class SaveOnProgressCallback:
   def __init__(self, save_path):
     """Callback that takes a model as an input and saves it if the error has decreased."""
-    self.min_error = 99999
+    self.min_error = 1e8
     self.save_count = 0
     self.save_path = save_path
   
   def __call__(self, model):
-    if model.error < self.min_error:
+    if not hasattr(model, 'val_error'):
+      raise AttributeError('Model has no validation loss. Make sure you are passing a model that has been trained with validation.')
+    
+    if model.val_error < self.min_error:
+      self.min_error = model.val_error
       current_save_path = self.save_path + '/' + str(self.save_count) + ".pkl"
       with open(current_save_path, 'wb') as f:
         pickle.dump(model, f)
