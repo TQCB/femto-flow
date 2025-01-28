9a18cfba9bc32c194dec0587aa42d27669181899
Raphael <raphael.pb.rialland@gmail.com>
2025-01-28 20:51:09 +0100

    vectorized embedding layer backward pass

--- femto_flow/layers.py
+++ femto_flow/layers.py
@@ -401,9 +401,12 @@ class PositionalEmbedding(Layer):
     d_global_embedding_weights = np.zeros_like(self.global_embedding_weights)
 
     # Iterate through the input sequence and corresponding output error
+    """
     for i, word_index in enumerate(self.input):
         # Add the output error (gradient) for the current word to the accumulated gradients
         d_global_embedding_weights[word_index] += output_error[i]
+    """
+    np.add.at(d_global_embedding_weights, self.input, output_error) 
     
     # Update global embedding weights using the accumulated gradients and learning rate
     # self.global_embedding_weights -= learning_rate * d_global_embedding_weights
